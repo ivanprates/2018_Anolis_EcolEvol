58875499fc0609e596d3737f1e92031a90413771
ivanprates <ivanprates@gmail.com>
2018-09-20 18:01:59 -0400

    Minor edits to improve code annotation.

--- run_fastqc.sh
+++ run_fastqc.sh
@@ -1,4 +1,4 @@
-### This script's goal is to run Fastqc to access read quality.
+### This script's goal is to run Fastqc to access raw read quality.
 ### By Ivan Prates, June 2018.
 ### Smithsonian National Museum of Natural History, Washington DC, USA.
 
