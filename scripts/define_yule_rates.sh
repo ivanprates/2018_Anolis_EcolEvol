23a83efdf795746395d38bf0f5d1e8a51f504429
ivanprates <ivanprates@gmail.com>
2018-09-21 16:10:27 -0400

    Improving code annotation.

--- scripts/define_yule_rates.sh
+++ scripts/define_yule_rates.sh
@@ -1,7 +1,10 @@
-# Estimating the Yule diversification rate using the Oak's pyule script
+### The goal of this Unix shell script is to estimate Yule diversification rates based on tree heights using the Oak's pyule script.
+### By Ivan Prates, June 2018.
+### Smithsonian National Museum of Natural History, Washington DC, USA.
+
 # for Anolis punctatus (npops = 3), height = 0.0039
 
-$ python /Users/dequeirk/Dropbox\ \(Smithsonian\)/ivan_lab/2018_Anolis_GEA/2017_Chapter_4/SNAPP/setting_priors/pyule/yule.py height 0.0039 3
+python /Users/dequeirk/Dropbox\ \(Smithsonian\)/ivan_lab/2018_Anolis_GEA/2017_Chapter_4/SNAPP/setting_priors/pyule/yule.py height 0.0039 3
 
 # Result:
 # ntips = 3
